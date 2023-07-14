0a966d800f1b70e608894eabb81e670a4531dad1
Ashesh <83585384+ashesh790@users.noreply.github.com>
2023-07-14 19:15:08 +0530

    Routing completed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropBoxComponent } from './prop-box/prop-box.component';
+import { Serv1Component } from './serv1/serv1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: PropBoxComponent
+  },
+  {
+    path: 'services', component: Serv1Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
